d9fcbc5ef7bc271d10acfa1568d8b666ce4c47b4
Sarfraz Ahmed Razi <sarfraz.razi291@gmail.com>
2020-03-08 14:11:48 +1100

    Latest changes

---
+++ gitScript.sh
@@ -0,0 +1,7 @@
+git init
+git config --global user.name "Sarfraz Ahmed Razi"
+git config --global user.email "sarfraz.razi291@gmail.com"
+git pull https://github.com/sarfrazrazi/test.git master
+git add .
+git commit -m "Latest changes"
+git push https://github.com/sarfrazrazi/test.git master
