043e9e6f3aa62e4cbbbfc1546c53314d5db4abfc
Sarfraz Ahmed Razi <sarfraz.razi291@gmail.com>
2020-03-08 16:45:54 +1100

    Testing user input

--- gitScript.sh
+++ gitScript.sh
@@ -3,5 +3,7 @@ git config --global user.name "Sarfraz Ahmed Razi"
 git config --global user.email "sarfraz.razi291@gmail.com"
 git pull https://github.com/sarfrazrazi/test.git master
 git add .
-git commit -m "Latest changes"
+read -p "Enter commit message: " COMMIT_MESSAGE
+
+git commit -m "$COMMIT_MESSAGE"
 git push https://github.com/sarfrazrazi/test.git master
